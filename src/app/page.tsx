777a5e07413e8214f4493a5534c7bac7f9360a1c
Contato Maestria <contato@maestrialaw.com.br>
2025-06-08 22:51:24 +0000

    mova a seção ebook para cima da de blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function LandingPage() {
         <TestimonialsSection />
         <FeaturesSection />
         <MaestriaFlixSection />
-        <BlogReferralSection />
         <EbookDownloadForm />
+        <BlogReferralSection />
       </main>
       <FooterSection />
     </div>
