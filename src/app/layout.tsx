1fb630556665de78c5552cdc4941e18f5d86e7cf
Contato Maestria <contato@maestrialaw.com.br>
2025-06-09 11:22:36 +0000

    tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,14 +25,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${notoSans.variable}`} suppressHydrationWarning>
-      <head>
-        <script dangerouslySetInnerHTML={{ __html: 'document.documentElement.setAttribute("data-cookieconsent", "ignore");' }} />
-      </head>
-      <body className="font-sans antialiased">
-        {children}
-        <Toaster />
-        <WhatsAppButton />
-      </body>
+ <head>
+ <script dangerouslySetInnerHTML={{ __html: 'document.documentElement.setAttribute("data-cookieconsent", "ignore");' }} />
+ <script
+ type="text/javascript"
+ dangerouslySetInnerHTML={{
+            __html: `
+ (function(c,l,a,r,i,t,y){
+ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+ t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/rkmzjdrdhj";
+ y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+ })(window, document, "clarity", "script", "rkmzjdrdhj");
+ `,
+          }}
+ />
+ {/* Google Tag Manager */}
+ <script type="text/javascript"
+ dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+ 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+ })(window,document,'script','dataLayer','GTM-WL9R9DLN');`,
+          }}
+ />
+ </head>
+ <body className="font-sans antialiased">
+ {children}
+ <Toaster />
+ <WhatsAppButton />
+ </body>
     </html>
   );
 }
